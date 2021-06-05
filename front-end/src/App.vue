f04e9d27a63002aa82c90580a6692ee3017a126f
rc <rc@hudl.us>
2021-06-05 10:36:58 -0700

    partial stepper implementation
    new lables

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -30,137 +30,167 @@
             <h1 class="display-1 font-weight-bold mb-3">
               Welcome to the Hawaii LFO Calculator
             </h1>
-            <div>
-              <v-stepper
-                v-model="el"
-                alt-labels
-                class="mt-12"
-              >
-                <v-stepper-header>
-                  <v-stepper-step 
-                    step="1"
-                    :complete="el > 1"
-                  >
-                    Submit charges
-                  </v-stepper-step>
+         </v-col>
+        </v-row>
+            <v-stepper v-model="e1">
+              <v-stepper-header>
+                <v-stepper-step
+                  :complete="e1 > 1"
+                  step="1"
+                >
+                  Name of step 1
+                </v-stepper-step>
 
-                  <v-divider></v-divider>
+                <v-divider></v-divider>
 
-                  <v-stepper-step 
-                    align="center" 
-                    step="2"
-                    :complete="el > 2"
-                  >
-                    Additional info
-                  </v-stepper-step>
-
-                  <v-divider></v-divider>
-
-                  <v-stepper-step 
-                    step="3"
-                  >
-                    Sentencing 
-                  </v-stepper-step>
-                </v-stepper-header>
-                <v-stepper-content step="1">
-                  <v-autocomplete
-                      v-model="charges"
-                      :disabled="isUpdating"
-                      :items="regulations"
-                      :rules="chargeSelectorRules"
-                      required
-                      filled
-                      chips
-                      clearable
-                      deletable-chips
-                      multiple
-                      color="primary"
-                      label="Select one or more charges to evaluate"
-                      item-text="regulation"
-                      item-value="section"
-                  >
-                    <template v-slot:selection="data">
-                      <v-chip
-                        v-bind="data.attrs"
-                        :input-value="data.selected"
-                        close
-                        @click="data.select"
-                        @click:close="remove(data.item)"
-                      >
-                        {{ data.item.regulation }}
-                      </v-chip>
-                    </template>
-                    <template v-slot:item="data">
-                      <template v-if="typeof data.item !== 'object'">
-                        <v-list-item-content v-text="data.item"></v-list-item-content>
-                      </template>
-                      <template v-else>
-                        <v-list-item-content>
-                          <v-list-item-title v-html="data.item.regulation"></v-list-item-title>
-                          <v-list-item-subtitle v-html="data.item.section"></v-list-item-subtitle>
-                        </v-list-item-content>
-                      </template>
-                    </template>
-                  </v-autocomplete>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      color="primary"
-                      @click="el = 2"
-                    >
-                      Continue
-                    </v-btn>
-                        <v-spacer></v-spacer>
-                </v-stepper-content>
-                <v-stepper-content
+                <v-stepper-step
+                  :complete="e1 > 2"
                   step="2"
                 >
-                  <v-container v-if="relevant != null">
-          <v-card
-            class="mx-auto"
-            max-width="1000"
-            tile
-            v-if="Object.keys(relevant.needs).length > 0"
-          >
-            <v-card-title>
-              General-purpose information required:
-            </v-card-title>
-              <v-text-field
-                value=""
-                label="Age of the defendant"
-                v-if="'age' in relevant.needs"
-              ></v-text-field>
-          </v-card>
-          <v-card
-            class="mx-auto"
-            tile
-            v-for="(s, section) in relevant.contextual"
-            v-bind:key="section"
-          >
-            <v-card-title>
-              {{ s.title }} (<a target="_blank" :href="s.url">{{ section }}</a>)
-            </v-card-title>
-            <v-subheader>Relevant regulations:</v-subheader>
-              <v-list-item two-line v-for="(relevant_s, relevant_section) in s.relevant" v-bind:key="relevant_section">
-                <v-list-item-content>
-                  <v-list-item-title>
-                    {{ relevant_s.title }} (<a target="_blank" :href="relevant_s.url">{{ relevant_section }}</a>)
-                  </v-list-item-title>
-                  <v-list-item-subtitle v-if="Object.keys(relevant_s.needs).length > 0">
-                    <v-checkbox v-if="'two_priors_past_five_years' in relevant_s.needs"
-                      label="Two identical offenses within past five years"
-                    >
-                    </v-checkbox>
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-          </v-card>
-        </v-container>
+                  Name of step 2
+                </v-stepper-step>
+
+                <v-divider></v-divider>
+
+                <v-stepper-step step="3">
+                  Name of step 3
+                </v-stepper-step>
+              </v-stepper-header>
+
+              <v-stepper-items>
+                <v-stepper-content step="1">
+                 <v-autocomplete
+                  v-model="charges"
+                  :disabled="isUpdating"
+                  :items="regulations"
+                  :rules="chargeSelectorRules"
+                  required
+                  filled
+                  chips
+                  clearable
+                  deletable-chips
+                  multiple
+                  color="primary"
+                  label="Select one or more charges to evaluate"
+                  item-text="regulation"
+                  item-value="section"
+              >
+                <template v-slot:selection="data">
+                  <v-chip
+                    v-bind="data.attrs"
+                    :input-value="data.selected"
+                    close
+                    @click="data.select"
+                    @click:close="remove(data.item)"
+                  >
+                    {{ data.item.regulation }}
+                  </v-chip>
+                </template>
+                <template v-slot:item="data">
+                  <template
+                    v-if="typeof data.item !== 'object'"
+                  >
+                    <v-list-item-content v-text="data.item"></v-list-item-content>
+                  </template>
+                  <template v-else>
+                    <v-list-item-content>
+                      <v-list-item-title v-html="data.item.regulation"></v-list-item-title>
+                      <v-list-item-subtitle v-html="data.item.section"></v-list-item-subtitle>
+                    </v-list-item-content>
+                  </template>
+                </template>
+              </v-autocomplete>
+                  <v-btn
+                    color="primary"
+                    @click="e1 = 2, computeNeeds()"
+                  >
+                    Continue
+                  </v-btn>
                 </v-stepper-content>
-              </v-stepper>
-              
-            </div>
-           </v-col>
-        </v-row>
+
+                <v-stepper-content step="2">
+                <v-container v-if="relevant != null">
+                  <v-card
+                    class="mx-auto"
+                    max-width="1000"
+                    tile
+                    v-if="Object.keys(relevant.needs).length > 0"
+                  >
+                    <v-card-title>
+                      General-purpose information required:
+                    </v-card-title>
+                    <v-form>
+                      <v-text-field
+                        value=""
+                        label="Age of the defendant"
+                        v-if="'age' in relevant.needs"
+                      ></v-text-field>
+                    </v-form>
+                  </v-card>
+                  <v-card
+                    class="mx-auto"
+                    max-width="1000"
+                    tile
+                    v-for="(s, section) in relevant.contextual"
+                    v-bind:key="section"
+                  >
+                    <v-card-title>
+                      {{ s.title }} (<a target="_blank" :href="s.url">{{ section }}</a>)
+                    </v-card-title>
+                    <v-subheader>Relevant regulations:</v-subheader>
+                    <v-form>
+                      <v-list-item two-line v-for="(relevant_s, relevant_section) in s.relevant" v-bind:key="relevant_section">
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            {{ relevant_s.title }} (<a target="_blank" :href="relevant_s.url">{{ relevant_section }}</a>)
+                          </v-list-item-title>
+                          <v-list-item-subtitle v-if="Object.keys(relevant_s.needs).length > 0">
+                            <v-checkbox v-if="'two_priors_past_five_years' in relevant_s.needs"
+                              label="Two identical offenses within past five years"
+                            >
+                            </v-checkbox>
+                          </v-list-item-subtitle>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-form>
+                  </v-card>
+                </v-container>
+
+
+                  <v-btn
+                    color="primary"
+                    @click="e1 = 3, computeNeeds()"
+                  >
+                    Continue
+                  </v-btn>
+
+                  <v-btn text>
+                    Cancel
+                  </v-btn>
+                </v-stepper-content>
+
+                <v-stepper-content step="3">
+                  <v-card
+                    class="mb-12"
+                    color="grey lighten-1"
+                    height="200px"
+                  ></v-card>
+
+                  <v-btn
+                    color="primary"
+                    @click="e1 = 1"
+                  >
+                    Continue
+                  </v-btn>
+
+                  <v-btn text>
+                    Cancel
+                  </v-btn>
+                </v-stepper-content>
+              </v-stepper-items>
+            </v-stepper>
+
+
       </v-container>
     </v-main>
   </v-app>
@@ -175,12 +205,12 @@ export default {
   data() {
     return {
       autoUpdate: true,
-      el: 1,
       charges: [],
       isUpdating: false,
       regulations: [],
       relevant: null,
       needs: {},
+      e1: 1,
       chargeSelectorRules: [
         value => !!value || 'Please select at least one regulation'
       ]
